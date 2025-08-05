554ed6d7bd4a954f5f63a0f357755622f6df22f5
Elchin <bayramlielcin03@gmail.com>
2025-08-05 20:30:55 +0400

    feat(Book): :hammer: Adding model serializer and API View for book model

--- book/serializers.py
+++ book/serializers.py
@@ -1,62 +1,82 @@
 from rest_framework import serializers
-from .models import Book
-from .models import Author, Category
+from .models import Book, Author, Category
 
-class BookListSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=10, decimal_places=2)
-    publication_date = serializers.DateField()
-    language = serializers.ChoiceField(choices=[
-        ('EN', 'English'),
-        ('AZ', 'Azerbaijani'),
-        ('TR', 'Turkish'),
-        ('RU', 'Russian'),
-    ], default='EN')
+# class BookListSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(max_length=255)
+#     price = serializers.DecimalField(max_digits=10, decimal_places=2)
+#     publication_date = serializers.DateField()
+#     language = serializers.ChoiceField(choices=[
+#         ('EN', 'English'),
+#         ('AZ', 'Azerbaijani'),
+#         ('TR', 'Turkish'),
+#         ('RU', 'Russian'),
+#     ], default='EN')
+
+class BookListModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['id','title','price', 'publication_date', 'language']
+        read_only_fields = ['id']
+
+# class BookSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(max_length=255)
+#     description = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+#     author = serializers.PrimaryKeyRelatedField(queryset=Author.objects.all())
+#     publication_date = serializers.DateField()
+#     categories = serializers.PrimaryKeyRelatedField(many=True, queryset=Category.objects.all(), required=False)
+#     price = serializers.DecimalField(max_digits=10, decimal_places=2)
+#     volume = serializers.IntegerField(default=1)
+#     language = serializers.ChoiceField(choices=[
+#         ('EN', 'English'),
+#         ('AZ', 'Azerbaijani'),
+#         ('TR', 'Turkish'),
+#         ('RU', 'Russian'),
+#     ], default='EN')
+#     stock_count = serializers.IntegerField(default=0)
+
+#     # def __init__(self, *args, **kwargs):
+#     #     from .models import Author, Category
+#     #     super().__init__(*args, **kwargs)
+#     #     self.fields['author'].queryset = Author.objects.all()
+#     #     self.fields['categories'].queryset = Category.objects.all()    
+
+#     def create(self, validated_data):
+#         categories = validated_data.pop('categories', [])
+#         book = Book.objects.create(**validated_data)
+#         book.categories.set(categories)
+#         return book
 
 
-class BookSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=255)
-    description = serializers.CharField(required=False, allow_blank=True, allow_null=True)
-    author = serializers.PrimaryKeyRelatedField(queryset=Author.objects.all())
-    publication_date = serializers.DateField()
-    categories = serializers.PrimaryKeyRelatedField(many=True, queryset=Category.objects.all(), required=False)
-    price = serializers.DecimalField(max_digits=10, decimal_places=2)
-    volume = serializers.IntegerField(default=1)
-    language = serializers.ChoiceField(choices=[
-        ('EN', 'English'),
-        ('AZ', 'Azerbaijani'),
-        ('TR', 'Turkish'),
-        ('RU', 'Russian'),
-    ], default='EN')
-    stock_count = serializers.IntegerField(default=0)
+#     def update(self, instance, validated_data):
+#         categories = validated_data.pop('categories', [])
+#         for key, value in validated_data.items():
+#             setattr(instance, key, value)
+#         instance.save()
+#         if categories:
+#             instance.categories.set(categories)
+#         return instance
 
-    # def __init__(self, *args, **kwargs):
-    #     from .models import Author, Category
-    #     super().__init__(*args, **kwargs)
-    #     self.fields['author'].queryset = Author.objects.all()
-    #     self.fields['categories'].queryset = Category.objects.all()    
+# """        setattr(instance,'title','Updated Title')
+#         instance.title = 'Updated Title'"""
 
-    def create(self, validated_data):
-        categories = validated_data.pop('categories', [])
-        book = Book.objects.create(**validated_data)
-        book.categories.set(categories)
-        return book
+class BookModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+        read_only_fields = ['id'] 
+        extra_kwargs = {
+            'price':{
+                'min_value':0
+            },
+            'publication_date':{'required':False, 'allow_null':True}
+
+        }
 
 
-    def update(self, instance, validated_data):
-        categories = validated_data.pop('categories', [])
-        for key, value in validated_data.items():
-            setattr(instance, key, value)
-        instance.save()
-        if categories:
-            instance.categories.set(categories)
-        return instance
 
-"""        setattr(instance,'title','Updated Title')
-        instance.title = 'Updated Title'"""
-        
+
    
 class AuthorListSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
