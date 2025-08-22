95ae68184ada5fef5666e76dd42883d4f9d31568
Elchin <bayramlielcin03@gmail.com>
2025-08-22 20:50:00 +0400

    Add stock management action to BookAdmin, implement signals for book creation and price validation, and update Book model save method.

--- book/admin.py
+++ book/admin.py
@@ -12,13 +12,18 @@ from .models import Category
 #     search_fields = ['title', 'author__name']
 #     ordering = ['-publication_date']
 
+def mark_as_depleted(modeladmin, request, queryset):
+    queryset.update(stock_count=0)
+
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ['title','author__name', 'publication_date', 'price', 'current_price', 'discount_percentage']
+    list_display = ['title','author', 'publication_date', 'price', 'current_price', 'discount_percentage', 'stock_count']
     list_filter = ['author', 'publication_date']
-    list_display_links = ['author__name']
-    search_fields = ['title', 'author__name']
+    list_display_links = ['author']
+    search_fields = ['title', 'author']
     ordering = ['-publication_date']
+    actions = [mark_as_depleted]
     # fields = ['title', 'author', 'publication_date', 'price', 'current_price', 'discount_percentage']
     # read_only_fields = ['current_price']
 
